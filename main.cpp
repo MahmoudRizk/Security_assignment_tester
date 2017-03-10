0c82c407ae540e831caffdf589ec988aa78295f7
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2017-03-11 01:50:26 +0200

    third commit

--- main.cpp
+++ main.cpp
@@ -5,16 +5,14 @@ using namespace std;
 
 int main(int argc, char*argv[]){
 
-  string q =argv[2]; //from python script.
   string p =argv[1]; //from python script
+  string q =argv[2]; //from python script.
 	string e =argv[3]; //from python script.
 
 	int x = stoi(p);
 	int y = stoi(q);
-	int z = stoi(e);
 
-  	int result = x + y ;
-    cout << to_string(result) << endl; //print in string format.
+  cout << x + y << endl; 
 
   return 0;
 }
